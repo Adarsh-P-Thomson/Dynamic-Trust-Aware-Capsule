29ea53710251fff3597965749f3628648465f7cc
Adarsh-P-Thomson <131739584+Adarsh-P-Thomson@users.noreply.github.com>
2025-07-22 12:19:01 +0530

    server basic running

--- Control Server/config/config.js
+++ Control Server/config/config.js
@@ -10,10 +10,10 @@ require('dotenv').config(); // Use .env file for environment variables
 module.exports = {
     port: process.env.PORT || 3001,
     postgres: {
-        user: process.env.PG_USER || 'your_postgres_user',
-        host: process.env.PG_HOST || 'localhost',
-        database: process.env.PG_DATABASE || 'dta_capsule',
-        password: process.env.PG_PASSWORD || 'your_postgres_password',
+        user: process.env.PG_USER || 'postgres' ,
+        host: process.env.PG_HOST || 'localhost' ,
+        database: process.env.PG_DATABASE  || 'dta_logs' ,
+        password: process.env.PG_PASSWORD || null ,
         port: process.env.PG_PORT || 5432,
     },
     mongo: {
